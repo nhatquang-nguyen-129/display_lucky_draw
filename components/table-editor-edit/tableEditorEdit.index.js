66b7ba6c37959ecd275043bd471265407c6d5af0
Nhat Quang Nguyen <nhatquang.nguyen.129@gmail.com>
2025-11-28 17:01:26 +0700

    commit

---
+++ components/table-editor-edit/tableEditorEdit.index.js
@@ -0,0 +1,16 @@
+// tableEditorEdit.entry.js
+// -----------------------------
+// Entry point tổng hợp cho TableEditor Edit module
+// -----------------------------
+
+// Import style (nếu có)
+import './tableEditorEdit.style.css';
+
+// Import interface (nếu có)
+import './tableEditorEdit.interface.js';
+
+// Import API helper
+import './tableEditorEdit.api.js';
+
+// Export main function
+export { initTableEditorEdit } from './tableEditorEdit.main.js';
---
+++ components/table-editor-file/tableEditorFile.index.js
@@ -0,0 +1,16 @@
+// tableEditorFile.entry.js
+// -----------------------------
+// Entry point tổng hợp cho TableEditor File module
+// -----------------------------
+
+// Import style (nếu có)
+import './tableEditorFile.style.css';
+
+// Import interface (nếu có)
+import './tableEditorFile.interface.js';
+
+// Import API helper
+import './tableEditorFile.api.js';
+
+// Export main function
+export { initTableEditorFile } from './tableEditorFile.main.js';
